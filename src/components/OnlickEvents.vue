60af31fc811a6ba2f154c92a525ffff04980938f
Majidshah83 <shahmajid508@gmail.com>
2022-07-25 17:34:44 +0500

    if else

--- src/components/OnlickEvents.vue
+++ src/components/OnlickEvents.vue
@@ -3,12 +3,19 @@
    <h1>helo</h1>
    <button v-on:click="helo">Helo Button</button>
    <button v-on:mouseover="helo2('i am majid shah')">click me </button>
+   <h1 v-if="show">condition true</h1>
+   <h1 v-else>condition false</h1>
+   <button v-on:click="display">toggle button</button>
     </div>
 </template>
 
 <script>
 export default {
     name:'OnclickEvents',
+    data()
+        {
+            return{show:true}
+        },
     methods:{
         helo()
         {
@@ -18,7 +25,13 @@ export default {
         helo2(data)
         {
             alert(data)
+        },
+        display()
+        {
+           this.show=!this.show;
         }
+        
+
     }
     
 }
